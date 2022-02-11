a31c5e56f9aab5b2f8ddad439e02fc2ae68f6c9e
G-Iarrusso <g.iarruso.dev@gmail.com>
2022-02-11 15:44:02 -0500

    comments working

--- CompilerV0.1.py
+++ CompilerV0.1.py
@@ -23,8 +23,6 @@ def main():
                 buffer1[current_char] = c
             elif c and current_char>=n:
                 buffer2[current_char - n] = c
-            if c == "\n":
-                current_char = current_char - 1
             current_char = current_char + 1
     with open("keywords.txt") as f:
         while True:
@@ -47,19 +45,22 @@ def main():
     print(buffer1[0:50])
     while token != "eof":
         token = buffer1[cnt]
-        if token != " " and token!="eof" and token!=";":
+        if token == "\n":
+           print("end of line")
+           lexeme = ""
+        elif  "//" in lexeme:
+            lexeme = lexeme + token
+            print(lexeme)
+        elif token != " " and token!="eof" and token!=";":
             lexeme = lexeme + token
         elif token == " " or token=="eof" or token== ";":
-            print("lexeme:" + lexeme)
-            print("prev:" + prev)
             if (lexeme in keywords):
                 print("keyword:" + lexeme)
             elif (lexeme in operators):
                 print("operator:" + lexeme)
             else:
                 if lexeme in symbol_table.keys():
-                    #Do nothing for right now
-                    
+                    #Do nothing for right now           
                     print("Do nothing for now")
                 elif lexeme not in symbol_table.keys() and (prev not in keywords):
                     print("non idenfied lexeme:" + lexeme)
@@ -70,12 +71,9 @@ def main():
                     print("")
 
                 #print(lexeme)
-            print("")
-            print("")
             prev = lexeme 
             lexeme = ""
         cnt = cnt + 1
-
         
 if __name__ == "__main__":
     main()
